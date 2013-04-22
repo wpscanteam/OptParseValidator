6e540ac3be65a65121c4fe78cf17e4fd3e516952
erwanlr <erwan.lr@gmail.com>
2013-04-22 11:37:22 +0200

    Main class renamed to reflect the gem name

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,4 @@
 
 require 'simplecov' if RUBY_VERSION >= '1.9'
 
-require File.expand_path(File.dirname(__FILE__) + '/../lib/custom_option_parser')
+require File.expand_path(File.dirname(__FILE__) + '/../lib/opt_parse_validator')
