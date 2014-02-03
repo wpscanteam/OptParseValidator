10c19b6639b82b756e48223192c6f1e377a992f5
erwanlr <erwan.lr@gmail.com>
2014-02-04 00:17:31 +0100

    Coverage service

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,5 +3,17 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 FIXTURES = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures'))
 
-require 'simplecov'
 require 'opt_parse_validator'
+
+if ENV['TRAVIS'] || ENV['COVERAGE']
+  require 'simplecov'
+
+  if ENV['TRAVIS']
+    require 'coveralls'
+    SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+  end
+
+  SimpleCov.start do
+    add_filter '/spec/'
+  end
+end
