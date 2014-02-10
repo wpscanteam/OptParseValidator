0394ed226eaf3672e527cd15780ee3e66a665f35
erwanlr <erwan.lr@gmail.com>
2014-02-10 16:53:54 +0100

    OptProxy added

---
+++ lib/opt_parse_validator/opts/opt_proxy.rb
@@ -0,0 +1,7 @@
+# encoding: utf-8
+
+module OptParseValidator
+  # Implementation of the Proxy Option
+  class OptProxy < OptURI
+  end
+end
---
+++ spec/lib/opt_parse_validator/opts/opt_proxy_spec.rb
@@ -0,0 +1,14 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe OptParseValidator::OptProxy do
+
+  subject(:opt) { OptParseValidator::OptProxy.new(['--proxy PROXY'], attrs) }
+  let(:attrs)   { { protocols: %w{http https socks socks5 socks4} } }
+
+  describe '#validate' do
+    # Handle by OptURI
+  end
+
+end
