3499e1924a22f13c8092e343dd8f2ba309ca7b22
Erwan <erwanlr@users.noreply.github.com>
2025-02-23 10:32:42 -0500

    Fixes Rubocop Offense

--- lib/opt_parse_validator.rb
+++ lib/opt_parse_validator.rb
@@ -25,7 +25,7 @@ module OptParseValidator
       @symbols_used = []
       @opts         = []
 
-      super(banner, width, indent)
+      super
     end
 
     # @return [ OptParseValidator::ConfigFilesLoaderMerger ]
